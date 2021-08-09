8211aa69390be1c9a0ff3d30eb3d83d823b6cc2e
wenming <wenming@ecquaria.com>
2021-08-09 18:03:19 +0800

    前端测试登录ok

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -2,27 +2,72 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Login;
 import com.example.demo.service.LoginService;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/user")
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class LoginController{
+
+    @Resource
+    private HttpSession session;
+
+    String SESSION_KEY =  "username";
 
     @Autowired
     private LoginService loginService;
 
-    @GetMapping("/select")
-    public Login getLogin(@Param("userName") String userName ){
-
-        return loginService.getLogin(userName);
-
+    @GetMapping("/")
+    public String indexPage(){
+        return "view/index";
     }
 
-    @PostMapping("/login")
-    public Login toLogin(@RequestBody Login login){
+    @GetMapping("/login")
+    public String loginPage(){
+        return "login";
+    }
+
+//    @GetMapping("/select")
+//    public Login getLogin(@Param("userName") String userName ){
+//
+//        return loginService.getLogin(userName);
+//
+//    }
+
+    @GetMapping("/loginError")
+    public String loginError(){
+        return "loginError";
+    }
+
+    @PostMapping("/webLoginVerify")
+    public String webLogin(@RequestParam("userName") String userName, @RequestParam("userPass") String userPass, Model model){
+        Login userLogin = loginService.toLogin(userName, userPass);
+        if (userLogin!=null){
+            session.setAttribute(SESSION_KEY,userName);
+            model.addAttribute(SESSION_KEY,userName);
+            return "view/index";
+        }else{
+            return "redirect:/loginError";
+        }
+    }
+
+    @PostMapping("/phoneLoginVerify")
+    public Login phoneLogin(@RequestBody Login login){
+        Login userLogin = loginService.toLogin(login.getUserName(), login.getUserPass());
+        return userLogin;
+    }
+
+
+//    @PostMapping("/loginVerify")
+//    public Login toLogin(@RequestBody Login login){
 //        System.out.println(login.getUserName()+":"+login.getUserPass());
-        return loginService.toLogin(login.getUserName(), login.getUserPass());
-    }
+//        return loginService.toLogin(login.getUserName(), login.getUserPass());
+//    }
+
+
 }
+
